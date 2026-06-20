87405bd66b17e910328e0c5e734636f692648cd7
Shagun Singh <shagun301106@gmail.com>
2026-06-20 21:33:52 +0530

    Upload multiple pdf ooption

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import streamlit as st
-
+from dotenv import load_dotenv
 
 
 def main():
-  st.set_page_config(page_title ="Chat with multiple PDFs", page_icon="books:")
-
-  st.header("Chat with multiple PDFs : books:")
+  load_dotenv()
+  st.set_page_config(page_title ="Chat with multiple PDFs", page_icon="📚")
+  
+  st.header("Chat with multiple PDFs : 📚")
   st.text_input("Ask a question about your document:")
 
   with st.sidebar:
      st.subheader("Your document")
-     st.file_uploader("Upload your PDFs here and click on Process")
-     st.button("Process")
-     
+     pdf_docs = st.file_uploader(
+        "Upload your PDFs here and click on Process", accept_multiple_files=True)
+     if st.button("Process"):
+       with st.spinner("Processing"):
+       # get pdf text
+        raw_text = get_pdf_text(pdf_docs)
 
 
 if __name__ == '__main__':
