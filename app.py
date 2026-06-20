cfad296870f230544b9f2d88093b90d712098c5e
Shagun Singh <shagun301106@gmail.com>
2026-06-20 21:46:29 +0530

    Extracting text from pdfs

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
 
+
+def get_pdf_text(pdf_docs):
+  text = ""
+  for pdf in pdf_docs:
+    pdf_reader = PdfReader(pdf)
+    for page in pdf_reader.pages:
+      text += page.extract_text()
+  return text
+  
 
 def main():
   load_dotenv()
